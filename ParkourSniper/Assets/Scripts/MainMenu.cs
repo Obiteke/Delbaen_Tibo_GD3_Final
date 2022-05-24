0886cc081353ec1207babab0a358c907fa4085c5
Obiteke <thibo.delbaen@gmail.com>
2022-05-24 15:24:38 +0200

    intro screen + more UI

---
+++ ParkourSniper/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    #region Methods
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
